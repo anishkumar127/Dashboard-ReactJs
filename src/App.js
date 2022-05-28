84c485d0198d2184fd452e5380a0b135f95f3e33
anish <anishbishnoi127@gmail.com>
2022-05-28 16:34:23 +0530

    routes added.

--- src/App.js
+++ src/App.js
@@ -58,24 +58,21 @@ const App = () => {
               <Route path="/employees" element="Employees" />
               <Route path="/customers" element="Customers" />
               {/* Apps */}
-              <Route path="/kanban" element="kanban"/>
-              <Route path="/editor" element="Editor"/>
+              <Route path="/kanban" element="kanban" />
+              <Route path="/editor" element="Editor" />
 
-              <Route path="calendar" element="Calendar"/>
-              <Route path="color-picker" element="ColorPicker"/>
+              <Route path="calendar" element="Calendar" />
+              <Route path="color-picker" element="ColorPicker" />
 
-{/* Charts  */}
-<Route path="/line" element="/Line"/>
-<Route path="/area" element="/Area"/>
-<Route path="/bar" element="/Bar"/>
-<Route path="/pie" element="/Pie"/>
-<Route path="/financial" element="Financial"/>
-<Route path="color-mapping" element="ColorMapping"/>
-<Route path="/pyramid" element="Pyramid"/>
-<Route path="/stacked" element="Stacked"/>
-
-
-
+              {/* Charts  */}
+              <Route path="/line" element="/Line" />
+              <Route path="/area" element="/Area" />
+              <Route path="/bar" element="/Bar" />
+              <Route path="/pie" element="/Pie" />
+              <Route path="/financial" element="Financial" />
+              <Route path="color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
             </Routes>
           </div>
         </div>
